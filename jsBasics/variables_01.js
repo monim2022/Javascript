a1321ab6b2bf1c3efb59720326adccdf5194aba5
Monim Mia <63046947+monim2022@users.noreply.github.com>
2023-09-07 17:28:06 +0000

    This is a variables of javascript

---
+++ jsBasics/variables_01.js
@@ -0,0 +1,12 @@
+const accountId = 1233232
+let accountEmail = "asdf@gmail.com"
+var accountPass = "12334"
+accountCity = "Dhaka"
+let accountState;
+
+//accountId = 2 not allowed
+accountEmail = "jkl@gmail.com"
+accountPass = "3344"
+accountCity = "Kishoreganj"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
