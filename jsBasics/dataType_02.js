20ba3de7cae4449e30b9011e09069b263919da98
Monim Mia <63046947+monim2022@users.noreply.github.com>
2023-09-07 17:53:54 +0000

    data type of javascript

---
+++ jsBasics/dataType_02.js
@@ -0,0 +1,16 @@
+let name = "Monim"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean = true or false
+//null =>standalone value
+//undefined
+//symbol =>unique
+
+
+//object
+//
+console.table(typeof[null,undefined])
